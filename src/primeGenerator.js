19c9d351b7d116132bf77202574c13fcae530c10
stefano <obsidianspam@gmail.com>
2018-10-25 11:14:16 +0100

    failed attempt using objects instead of arrays

--- src/primeGenerator.js
+++ src/primeGenerator.js
@@ -5,27 +5,40 @@ module.exports = {
 		// gonna worry about multiple calls performances
 
     const limit = 42949672//95 //224737*224737
-    const sieve = new Array(30000).fill(1)
-    sieve[0] = 0 // set the number 0 to not prime
-    sieve[1] = 0 // set the number 1 to not prime
+    const sieve = {
+      0: 0,
+      1: 0,
+      2: 1
+    }
+    for (let i=3; i<limit; i++) {
+      sieve[i] = 1
+    }
 
-    const upperLimit = Math.sqrt(limit)
+
+
+    // const upperLimit = Math.sqrt(limit)
     const output = []
 
-    sieve
-      .forEach((el, i)=>{
-        if (el) {
-          for (let j = i * i; j < limit; j += i) {
-            sieve[j] = 0
-          }
-        }
-      })
+    // while(output.length<n) {
+    //   const nextPrime = parseInt(Object.keys(sieve).find(key=>sieve[key]))
 
-    for (let i = 2; i < limit && output.length<n; i++) {
-        if(sieve[i]) {
-            output.push(i)
-        }
-    }
+    //   console.log(parseInt(nextPrime))
+    //   output.push(nextPrime)
+    // }
+    // sieve
+    //   .forEach((el, i)=>{
+    //     if (el) {
+    //       for (let j = i * i; j < limit; j += i) {
+    //         sieve[j] = 0
+    //       }
+    //     }
+    //   })
+
+    // for (let i = 2; i < limit && output.length<n; i++) {
+    //     if(sieve[i]) {
+    //         output.push(i)
+    //     }
+    // }
 
     return output
 	}
