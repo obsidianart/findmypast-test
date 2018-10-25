e45a4fb38f0cbaeb95dbb57025793938186e24e7
stefano <obsidianspam@gmail.com>
2018-10-25 10:23:32 +0100

    prime generation tests

---
+++ src/primeGenerator.js
@@ -0,0 +1,8 @@
+module.exports = {
+	generatePrime: (limit)=>{
+		// I would probably use a memoization pattern here but
+		// since I would just use a file for primes I'm not
+		// gonna worry about multiple calls performances
+		return []
+	}
+}
---
+++ tests/primeGenerator.js
@@ -0,0 +1,20 @@
+const test = require('tape')
+const primeGenerator = require('../src/primeGenerator')
+
+test('Generate 1 prime number', function (t) {
+	const primes = primeGenerator.generate(1)
+	const expected = [2]
+	t.same(expected, primes)
+})
+
+test('Generate 46 prime number', function (t) {
+	const primes = primeGenerator.generate(1)
+	const expected = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199]
+	t.same(expected, primes)
+})
+
+test('Generate 30000 prime numbers', function (t) {
+	const primes = primeGenerator.generate(1)
+	const expected = [2]
+	t.equal(451879, primes[30000])
+})
